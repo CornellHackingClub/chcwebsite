d305a070cd2330f6512328fd3dd1ace5bb60cb9a
Evan King <esk79@cornell.edu>
2016-07-17 20:46:06 -0700

    Added a small bit of content and added search bar

--- guides/models.py
+++ guides/models.py
@@ -8,7 +8,7 @@ class Guides(models.Model):
     description = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    document = models.FileField(upload_to='media/guides', blank=False)
+    document = models.FileField(null=False, blank=False)
 
     def __unicode__(self):
         # This is the title of the lecture that will display in the admin page
--- lectures/views.py
+++ lectures/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Lectures
+from django.db.models import Q
 
 # Just using function based views for now because they're easy
 def lectures_home(request):
     queryset = Lectures.objects.all()
+    search = request.GET.get("query")
+    if search:
+        queryset = queryset.filter(
+            Q(title__icontains=search) | Q(description__icontains=search))
     context = {
         "lectures": queryset,
     }
