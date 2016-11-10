79d021666c315c087838666e63ee747fc5e40006
Evan King <esk79@cornell.edu>
2016-11-10 14:07:02 -0500

    working on completion form

--- home/views.py
+++ home/views.py
@@ -41,50 +41,110 @@ def challenges(request):
     crypto_flag = request.POST.get("crypto_flag")
     forensic_flag = request.POST.get("forensic_flag")
     reverse_flag = request.POST.get("reverse_flag")
+
+    name = request.POST.get("name")
+    netid = request.POST.get("netid")
+    year = request.POST.get("year")
+
+    if name or netid or year:
+        return completed_challenge(request, name, netid, year)
+
     error = None
     error_text = "Sorry, not quite."
 
     if web_flag:
         print "got web flag" + web_flag
         if web_flag == os.getenv('WEB_FLAG', 0):
-              return form(request, 'web')
-        else: error = error_text
+            response = render(request, "completion_form.html")
+            response.set_cookie('web_flag', web_flag)
+            return response
+        else:
+            error = error_text
 
     if crypto_flag:
         print "got crypto flag" + crypto_flag
         if crypto_flag == os.getenv('CRYPTO_FLAG', 0):
-             return form(request, 'crypto')
-        else: error = error_text
+            response = render(request, "completion_form.html")
+            response.set_cookie('crypto_flag', crypto_flag)
+            return response
+        else:
+            error = error_text
 
     if forensic_flag:
         print "got forensic flag" + forensic_flag
-        if forensic_flag == os.getenv('FORENSIC_FLAG',0) or True:
-            return form(request, 'forensics')
-        else: error = error_text
+        if forensic_flag == os.getenv('FORENSIC_FLAG', 0) or True:
+            response = render(request, "completion_form.html")
+            response.set_cookie('forensic_flag', forensic_flag)
+            return response
+        else:
+            error = error_text
 
     if reverse_flag:
         print "got reverse flag" + reverse_flag
         if reverse_flag == os.getenv('REVERSE_FLAG', 0):
-             return form(request, 'reversing')
-        else: error = error_text
+            response = render(request, "completion_form.html")
+            response.set_cookie('reverse_flag', reverse_flag)
+            return response
+        else:
+            error = error_text
 
     context = {
         "error": error,
     }
     return render(request, "challenge.html", context)
 
+# def form(request, challenge):
+#     name = request.POST.get("name")
+#     netid = request.POST.get("netid")
+#     year = request.POST.get("year")
+#     error = None
+#     if name and netid and year:
+#         query = Form(name = name , netid = netid, year = year, challenge = challenge)
+#         query.save()
+#     elif name or netid or year:
+#         error ="Please fill out all fields."
+#     context = {
+#         "error": error,
+#     }
+#     return render(request, "completion_form.html", context)
 
-def form(request, challenge):
-    name = request.POST.get("name")
-    netid = request.POST.get("netid")
-    year = request.POST.get("year")
+def completed_challenge(request, name, netid, year):
+    web = request.COOKIES.get('web_flag', None)
+    crypto = request.COOKIES.get('crypto_flag', None)
+    forensic = request.COOKIES.get('forensic_flag', None)
+    reverse = request.COOKIES.get('reverse_flag', None)
+    challenge = check_flag(web, crypto, forensic, reverse)
+
     error = None
-    if name and netid and year:
-        query = Form(name = name , netid = netid, year = year, challenge = challenge)
+    thanks = None
+    if name and netid and year and challenge:
+        query = Form(name=name, netid=netid, year=year, challenge=challenge)
         query.save()
-    elif name or netid or year:
-        error ="Please fill out all fields."
+        thanks = "Thank you and great job! You'll be hearing from us shortly"
+    elif name or netid or year  or challenge:
+        error = "Please fill out all fields."
     context = {
         "error": error,
+        "thanks": thanks,
     }
-    return render(request, "completion_form.html", context) #redirect to thank you message and remove cookie
+    return render(request, "completion_form.html", context)
+
+
+def check_flag(web_flag, crypto_flag, forensic_flag, reverse_flag):
+    if web_flag:
+        if web_flag == os.getenv('WEB_FLAG', 0):
+            return "web"
+
+    if crypto_flag:
+        if crypto_flag == os.getenv('CRYPTO_FLAG', 0):
+            return "crypto"
+
+    if forensic_flag:
+        if forensic_flag == os.getenv('FORENSIC_FLAG', 0) or True:
+            return "forensics"
+
+    if reverse_flag:
+        if reverse_flag == os.getenv('REVERSE_FLAG', 0):
+            return "reversing"
+
+    return None
