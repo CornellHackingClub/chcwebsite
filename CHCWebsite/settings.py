2eb3be30270a21ef335019ea7e13bdfef91f464f
Evan King <esk79@cornell.edu>
2016-11-10 20:15:08 -0500

    turned off debug

--- CHCWebsite/settings.py
+++ CHCWebsite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jitdu3lmibz-)!bl_dpeg7ame8fu$4=*)omsse^nz%e+(ibmj*'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
