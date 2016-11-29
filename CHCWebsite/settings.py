0e57fb6770c45056354d645f4956fb71ea19e56b
Evan King <esk79@cornell.edu>
2016-11-29 18:24:47 -0500

    trying to fix media

--- CHCWebsite/settings.py
+++ CHCWebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
