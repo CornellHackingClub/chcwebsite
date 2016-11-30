4ce88ccfc7991d575fb2c4327bc31b06efd36080
Evan King <esk79@cornell.edu>
2016-11-30 17:33:20 -0500

    debug false

--- CHCWebsite/urls.py
+++ CHCWebsite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from django.conf.urls import patterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
