3852d437ba9c904637b04fd1a560554684b71b02
Evan King <esk79@cornell.edu>
2016-12-01 00:04:42 -0500

    media

--- CHCWebsite/urls.py
+++ CHCWebsite/urls.py
@@ -39,4 +39,5 @@ admin.site.site_header = 'Cornell Hacking Club Admin Page'
 if settings.DEBUG == True:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+else:
+    urlpatterns += [ url(r'^media/(?P<path>.*)$', serve, { 'document_root': settings.MEDIA_ROOT, }), url(r'^static/(?P<path>.*)$', serve, { 'document_root': settings.STATIC_ROOT }), ]    
