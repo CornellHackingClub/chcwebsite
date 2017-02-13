4886cc0277dee9e5bdded17fe3ad498014249747
Jimmy Cao <jc2462@cornell.edu>
2017-02-12 19:18:56 -0500

    fix bug because typing is too strong

--- CHCWebsite/log.py
+++ CHCWebsite/log.py
@@ -3,7 +3,7 @@ try:
     class CHCLogHandler(JournalHandler):
         def emit(self, record):
             if record.exc_info:
-                record.exc_info = ", ".join(record.exc_info)
+                record.exc_info = str(record.exc_info)
             super(CHCLogHandler, self).emit(record)
 except ImportError:
     from logging import NullHandler
