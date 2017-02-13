8b0442c3c077308738e72e6ebe463e47ab6b9bca
Jimmy Cao <jc2462@cornell.edu>
2017-02-12 19:27:37 -0500

    Revert those changes.

--- CHCWebsite/log.py
+++ CHCWebsite/log.py
@@ -1,10 +1,7 @@
 try:
     from systemd.journal import JournalHandler
     class CHCLogHandler(JournalHandler):
-        def emit(self, record):
-            if record.exc_info:
-                record.exc_info = str(record.exc_info)
-            super(CHCLogHandler, self).emit(record)
+        pass
 except ImportError:
     from logging import NullHandler
     CHCLogHandler = NullHandler
